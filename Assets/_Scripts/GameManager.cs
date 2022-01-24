0347be61ec852d56f20e4b37c4ee4bde23abcefa
alanfeiyuchang <alanfeiyuchang@gmail.com>
2022-01-23 19:08:01 -0800

    helper function added

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -78,7 +78,6 @@ public class GameManager : MonoBehaviour
             else if (currentState == GameState.Pausing)
                 SwitchGameState(GameState.Playing);
         }
-
     }
 
     public void SwitchGameState(GameState state)
